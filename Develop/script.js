b2628e99bb2a337b580699eaf91f2fc4867390ec
Acadia Huber <horsinaround235@gmail.com>
2022-06-11 12:47:47 -0400

    adding prompts

--- Develop/script.js
+++ Develop/script.js
@@ -1,8 +1,24 @@
 // Assignment code here
-
+var userPassword = "";
+var lowercase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j"]
+var uppercase = ["A", "B", "C", "D"]
+var numbers = ["0", "1"]
+var special = ["!", "@"]
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
+
+function generatePassword () {
+
+  window.prompt("Enter password between 8 - 128 characters.");
+  window.prompt("Would you like lowercase letters?");
+  window.prompt("Would you like uppercase letters?");
+  window.prompt("Would you like numbers?");
+  window.prompt("Would you like special characters?");
+
+
+}
+
 
 // Write password to the #password input
 function writePassword() {
