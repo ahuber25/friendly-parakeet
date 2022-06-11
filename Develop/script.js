4be6554e0f87cbc6615b95715c956e25980c56e1
Acadia Huber <horsinaround235@gmail.com>
2022-06-11 13:06:11 -0400

    password choices

--- Develop/script.js
+++ Develop/script.js
@@ -1,16 +1,16 @@
 // Assignment code here
 var userPassword = "";
-var lowercase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j"]
-var uppercase = ["A", "B", "C", "D"]
-var numbers = ["0", "1"]
-var special = ["!", "@"]
+var lowercase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var uppercase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+var numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+var special = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")"];
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
 function generatePassword () {
 
-  window.prompt("Enter password between 8 - 128 characters.");
+  window.prompt("Enter password length between 8 - 128 characters.");
   window.prompt("Would you like lowercase letters?");
   window.prompt("Would you like uppercase letters?");
   window.prompt("Would you like numbers?");
@@ -24,6 +24,8 @@ function generatePassword () {
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
+
+
 
   passwordText.value = password;
 
