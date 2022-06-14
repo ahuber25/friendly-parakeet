10434debe6677b483d1dbbdac0470279005807f1
Acadia Huber <horsinaround235@gmail.com>
2022-06-14 09:56:51 -0400

    moving funcitons around and adding math

--- Develop/script.js
+++ Develop/script.js
@@ -2,13 +2,14 @@
 var userPassword = "";
 var lowercase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 var uppercase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
-var numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+var numerical = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 var special = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")"];
+var options = "";
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
-function generatePassword () {
+function passwordOptions () {
 
   var length = Number(prompt("Enter password length between 8 - 128 characters."));
 
@@ -37,6 +38,35 @@ function generatePassword () {
     return null;
   }
 
+  var passwordChoices = {
+    length: length,
+    lowercase: lowercase,
+    uppercase: uppercase,
+    numerical: numerical,
+    special: special
+  }
+
+  return passwordOptions;
+}
+
+var randomChoice = function(arr) {
+var randomOptions = Math.floor(Math.random() * arr.length);
+var randomElement = arr[randomOptions];
+}
+
+var generatePassword = function() {
+  var choices = passwordOptions();
+  var result = [];
+
+  var possible =[];
+  var absolute =[];
+
+  if (!choices) return null;
+
+  if (choices.uppercase) {
+    possible = possible.concat(special);
+    absolute.push(randomChoice(special));
+  }
 
 }
 
@@ -46,9 +76,9 @@ function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
-
-
   passwordText.value = password;
+
+
 
 }
 
